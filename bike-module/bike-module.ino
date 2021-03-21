daef3217064197b4b0b99ce77255348dd6315245
Naman Chauhan <chauhannaman98@gmail.com>
2021-03-22 00:02:12 +0530

    updated: lcd updates configured

--- bike-module/bike-module.ino
+++ bike-module/bike-module.ino
@@ -1,38 +1,87 @@
 // bike module set as AP to receive packets
 
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>  
 #include <WiFiUdp.h>
+#include "ESPAsyncWebServer.h"
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define ssid "Bike-Module-AP"
 #define password "qwerty123"
 #define PORT 4210   // local port to listen on
+#define SDA D2
+#define SCL D1
 
+AsyncWebServer server(80);
 WiFiUDP Udp;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 char incomingPacket[255];  // buffer for incoming packets
 char replyPacket[] = "Got message successfully!";  // a reply string to send back
+//unsigned long Interval = 20000;
+bool switchStatus = false;
 
 
 void setup()  {
   Serial.begin(115200);
   Serial.println();
 
+  Wire.begin(SDA, SCL);
+  lcd.begin(16, 2);   // initializing the LCD
+  lcd.backlight(); // Enable or Turn On the backlight
+  lcd.home();
+
   Serial.print("Setting soft-AP ... ");
+  WiFi.mode(WIFI_AP);
   Serial.println(WiFi.softAP(ssid, password) ? "Ready":"Failed!");
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
   
   Udp.begin(PORT);
-  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), PORT);
+  server.begin(); 
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.softAPIP().toString().c_str(), PORT);
 
   // LED pin to show client status
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+
+  lcd.clear();
+  lcd.print("System ready!");
+  delay(1000);
+  
+  lcd.clear();
 }
 
 
 void loop() {
   bool _client = client_status();
+  lcd.setCursor(0, 0);
+  lcd.print("Connection: ");
+  lcd.setCursor(12, 0);
   if(_client)  {
+//    Serial.printf("client: true\t");
+    lcd.print("Okay");
+    lcd.setCursor(0, 1);
+    lcd.print("Ignition: ");
     receive_packets();
+    if(switchStatus) {
+//      Serial.printf("packet: true\n");
+      lcd.setCursor(10, 1);
+      lcd.print("ON ");
+      digitalWrite(LED_BUILTIN, LOW); // turn led ON
+    }
+    else  {      
+//      Serial.printf("packet: false\n");
+      lcd.setCursor(10, 1);
+      lcd.print("OFF");
+      digitalWrite(LED_BUILTIN, HIGH);  //turn led OFF
+    }
+  }
+  else  {
+    lcd.print("Lost");
+    lcd.setCursor(0, 1);
+    lcd.print("Ignition: OFF");
+    switchStatus = false;
   }
 }
 
@@ -53,6 +102,7 @@ bool client_status() {
 void receive_packets()  {
 //  digitalWrite(LED_BUILTIN, LOW); // client connected
   int packetSize = Udp.parsePacket();
+//  Serial.printf("packetSize = %d\n", packetSize);
   if(packetSize) {
     // receive incoming UDP packets
     Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
@@ -61,13 +111,19 @@ void receive_packets()  {
       incomingPacket[len] = 0;
     }
     Serial.printf("UDP packet contents:%s\n", incomingPacket);
+    Serial.printf("true compare: %d\n", strncmp(incomingPacket, "true", 4));
+
   
-    if(strcmp(incomingPacket, "true")==10)  {
-      digitalWrite(LED_BUILTIN, LOW); // turn led ON
+    if(strncmp(incomingPacket, "true", 4)==0)  {
+      switchStatus = true;
+      
     }
-    else if(strcmp(incomingPacket, "false")==10) {
-      digitalWrite(LED_BUILTIN, HIGH);  //turn led OFF
+    else if(strncmp(incomingPacket, "false", 5)==0) {
+      switchStatus = false;
     }
+    
+    Serial.print("switchStatus: ");
+    Serial.println(switchStatus);
 
     // send back a reply, to the IP address and port we got the packet from
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
--- helmet-module/helmet-module.ino
+++ helmet-module/helmet-module.ino
@@ -5,7 +5,7 @@
 
 #define ssid "Bike-Module-AP"
 #define password "qwerty123"
-#define IP "192.168.4.2"
+#define IP "192.168.4.1"
 #define PORT 4210   // local port to listen on
 
 WiFiUDP Udp;
@@ -19,12 +19,14 @@ void setup() {
 
   // Connect to AP
   Serial.printf("Connecting to %s ", ssid);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println(" connected");
+  Udp.begin(PORT);
 }
 
 
@@ -39,3 +41,72 @@ void loop() {
     }
   }
 }
+
+//#include <ESP8266WiFi.h>
+//#include <WiFiUdp.h>
+//#include "ESPAsyncWebServer.h"
+//
+//#define PORT 4210
+//
+//const char* ssid = "ESP8266-Access-Point";
+//const char* password = "123456789";
+//char senderPacket[100];
+////const int senderPacket = 1;
+//
+//AsyncWebServer server(80);
+//WiFiUDP Udp;
+//
+//void setup() {
+//  // put your setup code here, to run once:
+//  Serial.begin(115200);
+//  Serial.println();
+//
+//  // Setting the ESP as an access point
+//  Serial.print("Setting AP (Access Point)…");
+//  WiFi.mode(WIFI_AP);
+//  // Remove the password parameter, if you want the AP (Access Point) to be open
+//  WiFi.softAP(ssid, password);
+//
+//  IPAddress IP = WiFi.softAPIP();
+//  Serial.print("AP IP address: ");
+//  Serial.println(IP);
+//
+//  server.begin(); 
+//
+//  pinMode(LED_BUILTIN, OUTPUT);
+//  digitalWrite(LED_BUILTIN, HIGH);
+//}
+//
+//void loop() {
+//  if(client_status()) {
+//    digitalWrite(LED_BUILTIN, LOW);
+//    while (Serial.available() > 0 ) {
+//      String inputString = Serial.readString();
+//      inputString.toCharArray(senderPacket, 100);
+//      if(Udp.beginPacket("192.168.4.2", PORT) &&
+//        Udp.write(senderPacket) &&
+//        Udp.endPacket())  {
+//        Serial.printf("Packet sent: %s\n", senderPacket);
+//      }
+//      delay(1000);
+//    }
+//  }
+//  else  {
+//    digitalWrite(LED_BUILTIN, HIGH);
+//  }
+//}
+//
+//bool client_status() {
+//  unsigned char number_client;  
+//  number_client= wifi_softap_get_station_num();
+//  
+////  Serial.print(" Total Connected Clients are = ");
+////  Serial.println(number_client);
+//
+//  if(number_client == 0)  {
+//    return false;
+//  }
+//  else if(number_client == 1) {
+//    return true;
+//  }
+//}
